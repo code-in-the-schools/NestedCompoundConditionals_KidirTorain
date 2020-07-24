12996d3bb9701cce3a5b17645fe12ec70d484fc1
Kidir Torain <kktorainnn@gmail.com>
2020-07-24 17:32:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#part1
+for i in range(0,5):
+  print(i)
+
+  print("outer for loop | i :")
+for j in range(0,5):
+  print("outer for loop | i : inner for loop | j :)
+#part2
+
+for b in range(0,9):
+ i = ("2,4,6,8")
+ j = ("2,4,6,8")
+print(i,j)
+print("these are both even")
+
+#part3
+
+for b in range(0,9):
+ i = ("1,3,5,7,9")
+j = ("1,3,5,7,9")
+print(i,j)
+print("these are both odd")
+
+
+
+
+print("enter a character...")
+print("enter a length...")
